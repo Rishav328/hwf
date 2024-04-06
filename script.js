33566bd290e0343b92cbd222931ac75140efb071
Aishwaray(Rishav)Tiwary <70231128+Rishav328@users.noreply.github.com>
2024-04-06 11:30:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,10 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+function popUp(index) {
+    htmls = [`<h1>Hello, World</h1>`, `<h2>Hello, World</h2>`, `<h3>Hello, World</h3>`, `<h4>Hello, World</h4>`, `<h5>Hello, World</h5>`, `<h6>Hello, World</h6>`]
+    Swal.fire({
+        html: htmls[index],
+    });
+}
